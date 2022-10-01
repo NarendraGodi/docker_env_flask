340663b4304fdb67c2052e83ce77f3bc6ae66b1b
narendragodi <narendragodi@gmail.com>
2022-10-01 13:16:43 +0530

    Changes 1

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask,render_template,request
 import os
 app = Flask(__name__)
 
-text_fill = os.getenv('TEXT_FILL',default=None)
+name = os.getenv('NAME')
+print(name)
 
 @app.route('/')
-def hostdisplay():
-    return render_template('index.html',text_fill=text_fill)
+def webapp():
+    return render_template('index.html',name=name)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
